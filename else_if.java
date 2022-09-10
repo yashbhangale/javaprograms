9ebe0154e328578361ae29515134963fa6398ebf
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-09-10 21:38:26 +0530

    Create else_if.java

---
+++ else_if.java
@@ -0,0 +1,33 @@
+package second;
+
+import java.util.Scanner;
+
+public class else_if {
+
+	public static void main(String[] args) {
+		
+		
+		int marks;
+		System.out.print("Enter marks: ");
+		Scanner obj=new Scanner(System.in);
+		marks=obj.nextInt();
+		
+		if(marks>=60 && marks<=100)
+		{
+			System.out.print("First Division");
+		}
+		else if(marks>=45 && marks<=60)
+		{
+			System.out.print("second Division");
+		}
+		else if(marks>=33 && marks<=45)
+		{
+			System.out.print("Third Division");
+		}
+		else
+		{
+			System.out.print("Fialed");
+		}
+	}
+
+}
